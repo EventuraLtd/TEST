2d6fa7fbf6c667a7d17f0740d022cb62065c6f26
Alex-Stott <alex.stott@eventura.com>
2023-07-21 18:55:12 +0100

    Update and rename test3.ps1 to test4.ps1

--- test4.ps1
+++ test4.ps1
@@ -2,6 +2,8 @@ Write-Output $data
 Write-Output "NAME>..."
 Write-Output $FirstName
 
+New-ADUser -Name $FirstName
+
 Write-Output "FirstName - $FirstName"
 Write-Output "LastName - $LastName"
 Write-Output "TicketID - $TicketID"
