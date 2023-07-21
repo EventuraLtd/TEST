d0ed327793827804b40c8825310e5ca2dec48362
Alex-Stott <alex.stott@eventura.com>
2023-07-21 18:41:45 +0100

    Update and rename test2.ps1 to test3.ps1

--- test2.ps1
+++
@@ -1,7 +0,0 @@
-Write-Log $data
-
-Write-Log $FirstName
-
-Write-Log "FirstName - $FirstName"
-Write-Log "LastName - $LastName"
-Write-Log "TicketID - $TicketID"
---
+++ test3.ps1
@@ -0,0 +1,7 @@
+Write-Output $data
+Write-Output "NAME>..."
+Write-Output $FirstName
+
+Write-Output "FirstName - $FirstName"
+Write-Output "LastName - $LastName"
+Write-Output "TicketID - $TicketID"
